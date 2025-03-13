30190b7af026c495c5badc8079f928baf57bbf5e
Suyash7987 <104486757+Suyash7987@users.noreply.github.com>
2025-03-13 18:21:24 +0530

    Update Footer.jsx

--- reactrouting copy/src/components/Footer.jsx
+++ reactrouting copy/src/components/Footer.jsx
@@ -1,13 +1,44 @@
 import React from 'react'
-
+import './Footer.css'
 function Footer() {
   return (
     <>
-      <div id="Footer">
-        <div className="foot">
-             
-        </div>   
-      </div> 
+     <div className="maincontent5">
+  
+                     <div id="main5in2">
+                      <h2>About us</h2>
+                      <h4>Our Heritage</h4>
+                      <h4>Coffee house</h4>
+                      <h4>Our Company</h4>
+                     </div>
+                     <div id="main5in3">
+                      <h2>Responsibility</h2>
+                      <h4>Learn More</h4>
+                      <h4>Diversity</h4>
+                      <h4>Community</h4>
+                      <h4>Ethical Sourcing</h4>
+                      <h4>Enviromental Stewardship</h4>
+                     </div>
+                     <div id="main5in4">
+                      <h2>Quick Links</h2>
+                      <h4>Privacy policy</h4>
+                      <h4>FAQs</h4>
+                      <h4>Terms and Conditions</h4>
+                      <h4>Customer service</h4>
+                      <h4>Starbucks rewards of the Day</h4>
+                      <h4>Delievery</h4>
+                      <h4>Season's Gifting</h4>
+                     </div>
+                     <div id="main5in5">
+                         <h2>Social Media</h2>
+                         <div id="main5in5in">
+                         <h4>Instagram</h4>
+                         <h4>Facebook</h4>
+                         <h4>X</h4>
+                         </div>
+                     </div>
+                     
+                </div>
     </>
   )
 }
