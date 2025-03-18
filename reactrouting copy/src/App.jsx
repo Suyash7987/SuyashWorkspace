927ab1dd1ab31afc000018a26fdc9e2c88dc9f70
Suyash7987 <104486757+Suyash7987@users.noreply.github.com>
2025-03-18 10:47:35 +0530

    Update App.jsx

--- reactrouting copy/src/App.jsx
+++ reactrouting copy/src/App.jsx
@@ -1,18 +1,26 @@
 import './App.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Contact from "./components/Contact";
-import Dashboard from "./components/Dashboard";
-import Login from "./components/Login";
 import Home from "./components/Home";
 import MyNav from "./components/MyNav";
 import About from "./components/About";
+import SignUp from './components/Signup';
+import ProtectedRoute from './components/ProtectedRoute';
+import Product from './components/Product';
+import Loading from './components/Loading';
+import Profile from './components/Profile';
+import { useState } from 'react';
+
 
 function App() {
+  const [userLogined, setUserLogined] = useState(
+    localStorage.getItem("userLogined") === "true"
+);
   const route = createBrowserRouter([
     {
       path: "/",
       element: <>
-      <MyNav />
+      <MyNav userLogined={userLogined} />
       <Home />
     </>,
     },
@@ -20,7 +28,7 @@ function App() {
       path: "/About",
       element: (
         <>
-          <MyNav />
+          <MyNav userLogined={userLogined} />
           <About />
         </>
       ),
@@ -29,17 +37,18 @@ function App() {
       path: "/Contact",
       element: (
         <>
-          <MyNav />
+          <MyNav userLogined={userLogined} />
           <Contact />
         </>
       ),
     },
     {
-      path: "/Dashboard",
+      path: "/Product",
       element: (
         <>
-          <MyNav />
-          <Dashboard />
+          <MyNav userLogined={userLogined} />
+           <Product />
+          
         </>
       ),
     },
@@ -47,8 +56,28 @@ function App() {
       path: "/Login",
       element: (
         <>
-          <MyNav />
-          <Login />
+          <MyNav userLogined={userLogined} />
+          <Loading userLogined={userLogined} setUserLogined={setUserLogined}/>
+        </>
+      ),
+    },
+    {
+      path: "/Signup",
+      element: (
+        <>
+          <MyNav userLogined={userLogined} />
+          <SignUp />
+        </>
+      ),
+    },
+    {
+      path: "/Profile",
+      element: (
+        <>
+          <MyNav userLogined={userLogined} />
+          <ProtectedRoute redirectTo='/Profile' userLogined={userLogined}>
+          <Profile userLogined={userLogined} setUserLogined={setUserLogined} />
+          </ProtectedRoute>
         </>
       ),
     },
